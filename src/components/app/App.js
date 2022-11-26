9a5361f5f80316a00b0566d045a8ddf920362150
Vetiver <dmitriy_serga@mail.ru>
2022-11-26 10:01:23 +0300

    добавлен футер и адаптив

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import Voting from '../voting/voting';
+import Footer from '../footer/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Voting/>
+      <Footer/>
     </div>
   );
 }
