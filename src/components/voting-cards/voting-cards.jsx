be52dc373cc4f27da81253edb935329516a4ce54
theverybusybee <v_antonchik@mail.ru>
2022-11-27 20:54:48 +0300

    small fixes

--- src/components/voting-cards/voting-cards.jsx
+++ src/components/voting-cards/voting-cards.jsx
@@ -3,20 +3,19 @@ import styles from "./voting-cards.module.css";
 function VotingCards({ candidateData }) {
   return (
     <li className={styles.card}>
-      <img className={styles.photo} src={candidateData.photo} alt={candidateData.name} />
+      <img
+        className={styles.photo}
+        src={candidateData.photo}
+        alt={candidateData.name}
+      />
       <div className={styles.content}>
         <p className={styles.name}>{candidateData.name}</p>
         <p className={styles.description}>{candidateData.description}</p>
       </div>
 
-      <a
-        className={styles.link}
-        rel="noreferrer"
-        target="_blank"
-        href="/"
-      >
+      <button className={styles.button} type="submit">
         Оставить голос
-      </a>
+      </button>
     </li>
   );
 }
