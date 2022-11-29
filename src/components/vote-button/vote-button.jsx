537795225d2d33e13f348dc5c499fbb65330a3ff
Vetiver <dmitriy_serga@mail.ru>
2022-11-29 13:31:44 +0300

    комит

--- src/components/vote-button/vote-button.jsx
+++ src/components/vote-button/vote-button.jsx
@@ -1,19 +1,31 @@
+
 import { useState } from "react";
 import { pollId } from "../../store/weVote/weVote";
 import styles from "./vote-button.module.css";
 import TelegramLoginButton from "react-telegram-login";
+import { useDispatch } from "react-redux";
+import { SET_USER_DATA } from "../../store/actions/user-data-reducer";
+import { ADD_VOTING_DATA } from "../../store/actions/reducer.js";
 
 function VoteButton({
   pollData,
   weVoteApi,
   websocketWeVoteApi,
   setPollData,
-  index
+  index,
 }) {
   const [isAuthorized, setAuthorized] = useState(false);
   const [isVoteSent, setVoteSent] = useState(false);
-  const [bulletin, setbulletin] = useState([0,0,0,0]);
-  const telegramBotName = 'we_vote_dev_bot' 
+  const [bulletin, setBulletin] = useState([0, 0, 0, 0]);
+  const telegramBotName = "we_vote_dev_bot";
+  const dispatch = useDispatch();
+  const getVote = async () => {
+    const pollData = await weVoteApi.polls.find(pollId);
+    return dispatch({ type: ADD_VOTING_DATA, payload: pollData });
+  };
+  // const handleTelegramResponse = (response) => {
+  //   dispatch({ type: SET_USER_DATA, payload: response });
+  // };
 
   const onAuth = async (user) => {
     const response = await weVoteApi.auth.loginTelegram(user, "ru");
@@ -21,30 +33,28 @@ function VoteButton({
     websocketWeVoteApi.connect(response);
     setAuthorized(true);
     weVoteApi.polls.find(pollId).then(setPollData);
+    getVote()
   };
+
   const setIndexVote = () => {
-    if(index == 0) {
-      setbulletin([1,0,0,0])
+    if (index === 0) {
+      setBulletin([1, 0, 0, 0]);
     }
-    if(index == 1) {
-
-      setbulletin([0,1,0,0])
+    if (index === 1) {
+      setBulletin([0, 1, 0, 0]);
     }
-    if(index == 2) {
-
-      setbulletin([0,0,1,0])
+    if (index === 2) {
+      setBulletin([0, 0, 1, 0]);
     }
-    if(index == 3) {
-
-      setbulletin([0,0,0,1])
+    if (index === 3) {
+      setBulletin([0, 0, 0, 1]);
     }
-  }
-console.log(bulletin)
+  };
 
   const onVote = async () => {
-    setIndexVote()
-    if(bulletin == [0,0,0,0]) {
-      setIndexVote()
+    setIndexVote();
+    if (bulletin == [0, 0, 0, 0]) {
+      setIndexVote();
     }
     const nodeConfig = await weVoteApi.node.getNodeConfig();
     await weVoteApi.polls.vote({
@@ -71,98 +81,15 @@ console.log(bulletin)
 
   return (
     <>
-      <button className={styles.button} type="submit" onClick={onVote}>
-        Оставить голос
-      </button>
-      {/* {isVoteSent && <div>Vote sent </div>} */}
+      {isVoteSent?  
+      <button className={styles.button} disabled={true} type="submit" onClick={onVote}>
+      Голос отправлен
+    </button>
+    :
+    <button className={styles.button} type="submit" onClick={onVote}>
+    Оставить голос
+  </button>}
     </>
   );
 }
 export default VoteButton;
-
-
-// import { useState } from "react";
-// import { pollId } from "../../store/weVote/weVote";
-// import styles from "./vote-button.module.css";
-// import TelegramLoginButton from "react-telegram-login";
-// import { useDispatch } from "react-redux";
-// import { SET_USER_DATA } from "../../store/actions/user-data-reducer";
-
-// function VoteButton({
-//   pollData,
-//   weVoteApi,
-//   websocketWeVoteApi,
-//   setPollData,
-//   index,
-// }) {
-//   const [isAuthorized, setAuthorized] = useState(false);
-//   const [isVoteSent, setVoteSent] = useState(false);
-//   const [bulletin, setBulletin] = useState([0, 0, 0, 0]);
-//   const telegramBotName = "we_vote_dev_bot";
-//   const dispatch = useDispatch();
-
-//   // const handleTelegramResponse = (response) => {
-//   //   dispatch({ type: SET_USER_DATA, payload: response });
-//   // };
-
-//   const onAuth = async (user) => {
-//     const response = await weVoteApi.auth.loginTelegram(user, "ru");
-//     weVoteApi.authorize(response);
-//     websocketWeVoteApi.connect(response);
-//     setAuthorized(true);
-//     weVoteApi.polls.find(pollId).then(setPollData);
-//   };
-
-//   const setIndexVote = () => {
-//     if (index === 0) {
-//       setBulletin([1, 0, 0, 0]);
-//     }
-//     if (index === 1) {
-//       setBulletin([0, 1, 0, 0]);
-//     }
-//     if (index === 2) {
-//       setBulletin([0, 0, 1, 0]);
-//     }
-//     if (index === 3) {
-//       setBulletin([0, 0, 0, 1]);
-//     }
-//   };
-
-//   const onVote = async () => {
-//     setIndexVote();
-//     if (bulletin == [0, 0, 0, 0]) {
-//       setIndexVote();
-//     }
-//     const nodeConfig = await weVoteApi.node.getNodeConfig();
-//     await weVoteApi.polls.vote({
-//       pollData,
-//       nodeConfig,
-//       bulletin: [bulletin],
-//       hasRemoteSeed: true,
-//     });
-//     setVoteSent(true);
-//   };
-
-//   if (isAuthorized) {
-//     return (
-//       <TelegramLoginButton
-//         botName={telegramBotName}
-//         buttonSize={"large"}
-//         cornerRadius={0}
-//         lang={"ru"}
-//         usePic={false}
-//         dataOnauth={onAuth}
-//       />
-//     );
-//   }
-
-//   return (
-//     <>
-//       <button className={styles.button} type="submit" onClick={onVote}>
-//         Оставить голос
-//       </button>
-//       {isVoteSent && <div>Vote sent </div>}
-//     </>
-//   );
-// }
-// export default VoteButton;
