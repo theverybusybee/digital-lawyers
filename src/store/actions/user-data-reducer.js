4ff2c5af58fd85adf375049397066ccb1ca6d6ea
theverybusybee <v_antonchik@mail.ru>
2022-11-29 12:52:23 +0300

    auth commit

---
+++ src/store/actions/user-data-reducer.js
@@ -0,0 +1,3 @@
+export const SET_USER_DATA = "SET_USER_DATA";
+export const SET_ON_AUTH = "SET_ON_AUTH";
+export const SET_IS_VOTED = "SET_IS_VOTED";
---
+++ src/store/reducers/user-data-reducer.js
@@ -0,0 +1,38 @@
+import {
+  SET_USER_DATA,
+  SET_ON_AUTH,
+  SET_IS_VOTED,
+} from "../actions/user-data-reducer";
+
+const initialState = {
+  userData: {},
+  onAuth: false,
+  isVoted: false,
+};
+
+const votingReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER_DATA: {
+      return {
+        ...state,
+        userData: { ...state, userData: action.payload.data },
+      };
+    }
+    case SET_ON_AUTH: {
+      return {
+        ...state,
+        userData: { ...state, onAuth: true },
+      };
+    }
+    case SET_IS_VOTED: {
+      return {
+        ...state,
+        userData: { ...state, isVoted: action.payload.result.voted },
+      };
+    }
+    default:
+      return state;
+  }
+};
+
+export default votingReducer;
